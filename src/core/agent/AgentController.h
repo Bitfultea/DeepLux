a9979b7d5061ae8fe31e31f4b875cf92022a801b
Hymson_Charles <hyh86780253@gmail.com>
2026-09-11 09:12:54 +0800

    fix: include complete agent message type

--- src/core/agent/AgentController.h
+++ src/core/agent/AgentController.h
@@ -1,6 +1,8 @@
 #ifndef DEEPLUX_AGENT_CONTROLLER_H
 #define DEEPLUX_AGENT_CONTROLLER_H
 
+#include "ILLMClient.h"
+
 #include <QJsonArray>
 #include <QJsonObject>
 #include <QMutex>
@@ -11,10 +13,8 @@ namespace DeepLux {
 
 class AgentObserver;
 class AgentActor;
-class ILLMClient;
 struct GuiEvent;
 struct AgentActionLogEntry;
-struct AgentMessage;
 
 class AgentController : public QObject {
     Q_OBJECT
